8b059a9cbb97dc4e05b8d92305b8a8e5cd3a025c
Kevin Tran <kevin1997.t@gmail.com>
2021-07-22 15:54:32 +0100

    Mark and Toys solution

--- HackerRankQuestions/src/MarkAndToys.java
+++ HackerRankQuestions/src/MarkAndToys.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class MarkAndToys {
 
@@ -8,6 +6,19 @@ class MarkAndToys {
     public static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        List<Integer> prices = Arrays.asList(1, 12, 5, 111, 200, 1000, 10);
+        int k = 50;
+        Collections.sort(prices);
+        List<Integer> purchases = new ArrayList<>();
+
+        for (Integer price : prices) {
+            if (k != 0 && k - price > 0 ) {
+                k = k- price;
+//                System.out.println("Balance left is"+ (k - price));
+                purchases.add(price);
+            }
+        }
+        System.out.println(purchases.size());
 
     }
 }
